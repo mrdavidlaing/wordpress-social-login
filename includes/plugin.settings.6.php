6827df302c957faf9aaacb5627e8add4bf823d8c
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-05 11:42:20 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@471124 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings.6.php
+++ includes/plugin.settings.6.php
@@ -6,6 +6,6 @@
 	Notes: 
 </p>
 <ul style="list-style:circle inside;margin-left:25px;">
-	<li style="color: #000000;font-size: 14px;">This is free software. Polite and descriptive questions will be given priority.</li>
-	<li style="color: #000000;font-size: 14px;">Underrating and tagging this plugin as broken are both fine, but a comment on the issue you had is appreciated. Thank you!</li>
+	<li style="color: #000000;">This is free software. Polite and descriptive questions will be given priority.</li>
+	<li style="color: #000000;">Underrating and tagging this plugin as broken are both fine, but a comment on the issue you had is appreciated. Thank you!</li>
 </ul>
