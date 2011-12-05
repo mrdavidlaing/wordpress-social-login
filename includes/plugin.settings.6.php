266ed6f1ecf3bc46a6ed1f262646cf2bfbe4eb4b
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-05 11:32:41 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@471119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings.6.php
+++ includes/plugin.settings.6.php
@@ -1,4 +1,4 @@
-<p style="margin-left:25px;margin:10px;">  
+<p style="margin-left:25px;margin:10px;font-size: 14px;">  
 	If you run into any issue, or have a feature request, the best way to reach me is at <b>hybridauth@gmail.com</b>
 	
 	<br />
