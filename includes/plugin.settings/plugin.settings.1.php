edc4ad1d556273d99d9593f4adebe4ade97e58d4
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-08 19:14:22 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@472567 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings/plugin.settings.1.php
+++ includes/plugin.settings/plugin.settings.1.php
@@ -72,9 +72,9 @@
 <p style="margin:10px;margin-top:30px;font-size: 14px;"> 
 	<strong>IMPORTANT</strong>:
 	<br />
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. This plugin is <b>NOT guaranteed</b> to work on every server,
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. This plugin is <b>100% guaranteed NOT to work on every server</b>,
 	<!--  and definitely not by idly wishing :D -->
-	and will also require some works on your side.
+	and worst, it will require some works on your side.
 	<br />
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. Please take a minute to read the <a href="options-general.php?page=wordpress-social-login&wslp=2">Plugin User Guide</a> and make sure to run the <a href="options-general.php?page=wordpress-social-login&wslp=3">Requirements Test</a> before.
 	<br />
