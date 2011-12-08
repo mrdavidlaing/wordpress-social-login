b5f600e89ddc70574a3922884683d950675642f0
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-08 10:22:09 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@472386 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hybridauth/Hybrid/Providers/Facebook.php
+++ hybridauth/Hybrid/Providers/Facebook.php
@@ -15,7 +15,7 @@
 class Hybrid_Providers_Facebook extends Hybrid_Provider_Model
 {
 	// default permissions, and alot of them. You can change them from the configuration by setting the scope to what you want/need
-	public $scope = "email, user_about_me, user_birthday, user_hometown, user_website";
+	public $scope = "email, user_about_me";
 
 	/**
 	* IDp wrappers initializer 
