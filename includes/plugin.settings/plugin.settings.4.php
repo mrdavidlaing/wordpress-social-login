fbc38f1fc0e75c61935e38ae296642f24aa8a743
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-10 14:23:44 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@473141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings/plugin.settings.4.php
+++ includes/plugin.settings/plugin.settings.4.php
@@ -41,7 +41,8 @@
     <h3 style="margin: 0 0 5px;">Need Help?</h3>
 
 	<p style="line-height: 19px;">
-		If you are still new to things, we recommend that you read the <a href="options-general.php?page=wordpress-social-login&wslp=2">Plugin User Guide</a>.
+		If you are still new to things, we recommend that you read the <a href="options-general.php?page=wordpress-social-login&wslp=2">Plugin User Guide</a> 
+		and to make sure your server settings meet this <a href="options-general.php?page=wordpress-social-login&amp;wslp=3">Plugin Requirements</a>.
 	</p>
 </div> 
 
