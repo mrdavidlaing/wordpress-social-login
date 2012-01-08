18efed03d98a52bbf3650e2996b8f25f478e3850
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-08 19:06:37 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@486608 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings/plugin.settings.1.php
+++ includes/plugin.settings/plugin.settings.1.php
@@ -62,11 +62,11 @@
 
 <?php 
 	// let give this a try
-	// and only if we have if up and running
+	// and only if we have it up and running
 	global $wpdb;
 	$sql = "SELECT count( * ) as items FROM `wp_usermeta` where meta_key = 'wsl_user' "; 
 	$rs = $wpdb->get_results( $sql );  
-	if( count( $rs ) && $rs[0]->items >= 5 ){
+	if( count( $rs ) && $rs[0]->items >= 3 ){
 ?>
 <div style="clear:both;padding-bottom: 0;" class="wsl_donate wsl_aside">
     <h3 style="margin: 0 0 5px;">Donate</h3>
