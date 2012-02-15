2b340f46cdd35ba36586316f16563927b2dadb62
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-15 17:30:19 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@505527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings/plugin.settings.4.php
+++ includes/plugin.settings/plugin.settings.4.php
@@ -166,7 +166,7 @@ Except for OpenID providers, each social network and identities provider will re
 					<?php if ( $provider_id == "Twitter" ) : ?>
 						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Put your website domain in the <b>Application Website</b> and <b>Application Callback URL</b> fields. This should match with the current hostname <em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>.</p> 
 						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Set the <b>Application Type</b> to <em style="color:#CB4B16;">Browser</em>.</p> 
-						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Set the <b>Default Access Type</b> to <em style="color:#CB4B16;">Read, Write, & Direct Messages</em>.</p> 
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Set the <b>Default Access Type</b> to <em style="color:#CB4B16;">Read</em>.</p> 
 					<?php endif; ?>	
 					
 					<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Once you have registered, copy and past the created application credentials into this setup page.</p>  
