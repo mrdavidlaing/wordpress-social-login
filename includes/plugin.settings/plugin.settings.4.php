0eccbafe9098949cb76c1ce924bca3337076d851
miled <miled@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-30 23:27:00 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wordpress-social-login/trunk@579514 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin.settings/plugin.settings.4.php
+++ includes/plugin.settings/plugin.settings.4.php
@@ -106,7 +106,7 @@ Except for OpenID providers, each social network and identities provider will re
 ?> 
 	<h3 style="margin-left:30px;"><img alt="<?php echo $provider_name ?>" title="<?php echo $provider_name ?>" src="<?php echo $assets_base_url . strtolower( $provider_id ) . '.png' ?>" style="vertical-align: top;width:16px;height:16px;" /> <?php echo ++$nbprovider ?>. <?php echo $provider_name ?></h3> 
 	
-	<?php if ( in_array( $provider_id, array( "Twitter", "LinkedIn", "Identica", "Steam" ) ) ) : ?>
+	<?php if ( in_array( $provider_id, array( "Twitter", "LinkedIn", "Identica", "Tumblr", "Goodreads", "500px", "Vkontakte", "Gowalla", "Steam" ) ) ) : ?>
 		<small>
 			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
 			<b  style="color:#CB4B16;">Note:</b> The <b><?php echo $provider_name ?></b> API do not return a user's email address. A random email will then be generated instead.
